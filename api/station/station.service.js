4e07a205491dba5ae26193fcc72b8264274b12bd
oshrag <oshra.greenblat@gmail.com>
2024-09-01 16:36:05 +0300

    add station endpoints

--- api/station/station.service.js
+++ api/station/station.service.js
@@ -8,10 +8,10 @@ import { asyncLocalStorage } from '../../services/als.service.js'
 const PAGE_SIZE = 3
 
 export const stationService = {
-    // remove,
+    remove,
     query,
-    // getById,
-    // add,
+    getById,
+    add
     // update,
     // addCarMsg,
     // removeCarMsg,
@@ -20,19 +20,27 @@ export const stationService = {
 async function query(filterBy = { txt: '' }) {
     try {
         const criteria = _buildCriteria(filterBy)
-        const sort = _buildSort(filterBy)
-
+       // const sort = _buildSort(filterBy)
+  
+       console.log('filterby:', filterBy)
+       console.log('criteria:', criteria)
+      
         const collection = await dbService.getCollection('station')
-        // var stationCursor = await collection.find(criteria, { sort })
+        var stationCursor = await collection.find(criteria)
         console.log('station.service query')
-        var stationCursor = await collection.find()
+        //var stationCursor = await collection.find()
 
 
-        if (filterBy.pageIdx !== undefined) {
-            stationCursor.skip(filterBy.pageIdx * PAGE_SIZE).limit(PAGE_SIZE)
-        }
+        // if (filterBy.pageIdx !== undefined) {
+        //     stationCursor.skip(filterBy.pageIdx * PAGE_SIZE).limit(PAGE_SIZE)
+        // }
 
         const stations = stationCursor.toArray()
+
+
+
+
+
         return stations
     } catch (err) {
         logger.error('cannot find stations', err)
@@ -40,53 +48,53 @@ async function query(filterBy = { txt: '' }) {
     }
 }
 
-// async function getById(carId) {
-//     try {
-//         const criteria = { _id: ObjectId.createFromHexString(carId) }
+async function getById(stationId) {
+    try {
+        const criteria = { _id: ObjectId.createFromHexString(stationId) }
 
-//         const collection = await dbService.getCollection('car')
-//         const car = await collection.findOne(criteria)
-//         console.log('car:', car)
-//         car.createdAt = car._id.getTimestamp()
-//         return car
-//     } catch (err) {
-//         logger.error(`while finding car ${carId}`, err)
-//         throw err
-//     }
-// }
+        const collection = await dbService.getCollection('station')
+        const station = await collection.findOne(criteria)
+        console.log('station:', station)
+        station.createdAt = station._id.getTimestamp()
+        return station
+    } catch (err) {
+        logger.error(`while finding station ${stationId}`, err)
+        throw err
+    }
+}
 
-// async function remove(carId) {
-//     const { loggedinUser } = asyncLocalStorage.getStore()
-//     const { _id: ownerId, isAdmin } = loggedinUser
+async function remove(stationId) {
+    const { loggedinUser } = asyncLocalStorage.getStore()
+    const { _id: ownerId, isAdmin } = loggedinUser
 
-//     try {
-//         const criteria = {
-//             _id: ObjectId.createFromHexString(carId),
-//         }
-//         if (!isAdmin) criteria['owner._id'] = ownerId
+    try {
+        const criteria = {
+            _id: ObjectId.createFromHexString(stationId),
+        }
+        if (!isAdmin) criteria['createdBy._id'] = ownerId
 
-//         const collection = await dbService.getCollection('car')
-//         const res = await collection.deleteOne(criteria)
+        const collection = await dbService.getCollection('station')
+        const res = await collection.deleteOne(criteria)
 
-//         if (res.deletedCount === 0) throw ('Not your car')
-//         return carId
-//     } catch (err) {
-//         logger.error(`cannot remove car ${carId}`, err)
-//         throw err
-//     }
-// }
+        if (res.deletedCount === 0) throw ('Not your station')
+        return stationId
+    } catch (err) {
+        logger.error(`cannot remove station ${stationId}`, err)
+        throw err
+    }
+}
 
-// async function add(car) {
-//     try {
-//         const collection = await dbService.getCollection('car')
-//         await collection.insertOne(car)
+async function add(station) {
+    try {
+        const collection = await dbService.getCollection('station')
+        await collection.insertOne(station)
 
-//         return car
-//     } catch (err) {
-//         logger.error('cannot insert car', err)
-//         throw err
-//     }
-// }
+        return station
+    } catch (err) {
+        logger.error('cannot insert station', err)
+        throw err
+    }
+}
 
 // async function update(car) {
 //     const carToSave = { vendor: car.vendor, speed: car.speed }
@@ -133,13 +141,23 @@ async function query(filterBy = { txt: '' }) {
 //     }
 // }
 
+
+
 function _buildCriteria(filterBy) {
-    const criteria = {
-        vendor: { $regex: filterBy.txt, $options: 'i' },
-        speed: { $gte: filterBy.minSpeed },
+
+   if (filterBy.createdBy) {
+        const criteria = {
+            $or: [
+                { 'createdBy.id': filterBy.createdBy }, 
+                { 'savedBy': { $in: [filterBy.createdBy] } } 
+            ]
+        }
+        return criteria
+    } else {
+        return {}
     }
 
-    return criteria
+   
 }
 
 function _buildSort(filterBy) {
