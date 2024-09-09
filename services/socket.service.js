d62cdf905ee631f4e2fe08e0ee2753a9c00bd7ca
eitany52 <eitanpowerful@gmail.com>
2024-09-09 17:41:32 +0300

    add sockets support

--- services/socket.service.js
+++ services/socket.service.js
@@ -1,7 +1,7 @@
 import { logger } from './logger.service.js'
 import { Server } from 'socket.io'
 
-var gIo = null
+let gIo = null
 
 export function setupSocketAPI(http) {
     gIo = new Server(http, {
@@ -10,21 +10,17 @@ export function setupSocketAPI(http) {
         }
     })
     gIo.on('connection', socket => {
-        logger.info(`New connected socket [id: ${socket.id}]`)
-        socket.on('disconnect', socket => {
-            logger.info(`Socket disconnected [id: ${socket.id}]`)
-        })
+        socket.on('disconnect', socket => { })
+
         socket.on('chat-set-topic', topic => {
             if (socket.myTopic === topic) return
             if (socket.myTopic) {
                 socket.leave(socket.myTopic)
-                logger.info(`Socket is leaving topic ${socket.myTopic} [id: ${socket.id}]`)
             }
             socket.join(topic)
             socket.myTopic = topic
         })
         socket.on('chat-send-msg', msg => {
-            logger.info(`New chat msg from socket [id: ${socket.id}], emitting to topic ${socket.myTopic}`)
             // emits to all sockets:
             // gIo.emit('chat addMsg', msg)
             // emits only to sockets in the same room
@@ -32,15 +28,12 @@ export function setupSocketAPI(http) {
         })
 
         socket.on('user-watch', userId => {
-            logger.info(`user-watch from socket [id: ${socket.id}], on user ${userId}`)
             socket.join('watching:' + userId)
         })
         socket.on('set-user-socket', userId => {
-            logger.info(`Setting socket.userId = ${userId} for socket [id: ${socket.id}]`)
             socket.userId = userId
         })
         socket.on('unset-user-socket', () => {
-            logger.info(`Removing socket.userId for socket [id: ${socket.id}]`)
             delete socket.userId
         })
 
@@ -57,12 +50,19 @@ async function emitToUser({ type, data, userId }) {
     const socket = await _getUserSocket(userId)
 
     if (socket) {
-        logger.info(`Emiting event: ${type} to user: ${userId} socket [id: ${socket.id}]`)
         socket.emit(type, data)
     } else {
-        logger.info(`No active socket for user: ${userId}`)
         // _printSockets()
     }
+}
+
+async function join({ room, userId }) {
+    const socket = await _getUserSocket(userId)
+    socket.join(room)
+}
+async function leave({ room, userId }) {
+    const socket = await _getUserSocket(userId)
+    socket.leave(room)
 }
 
 // If possible, send to all sockets BUT not the current socket 
@@ -70,19 +70,14 @@ async function emitToUser({ type, data, userId }) {
 async function broadcast({ type, data, room = null, userId }) {
     userId = userId.toString()
 
-    logger.info(`Broadcasting event: ${type}`)
     const excludedSocket = await _getUserSocket(userId)
     if (room && excludedSocket) {
-        logger.info(`Broadcast to room ${room} excluding user: ${userId}`)
         excludedSocket.broadcast.to(room).emit(type, data)
     } else if (excludedSocket) {
-        logger.info(`Broadcast to all excluding user: ${userId}`)
         excludedSocket.broadcast.emit(type, data)
     } else if (room) {
-        logger.info(`Emit to room: ${room}`)
         gIo.to(room).emit(type, data)
     } else {
-        logger.info(`Emit to all`)
         gIo.emit(type, data)
     }
 }
@@ -118,4 +113,6 @@ export const socketService = {
     // Send to all sockets BUT not the current socket - if found
     // (otherwise broadcast to a room / to all)
     broadcast,
+    join,
+    leave
 }
